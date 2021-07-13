942d9d2f8ec1b1ede3eff91c60bf186fff476367
jlehnGIT <64560045+jlehnGIT@users.noreply.github.com>
2021-07-12 21:51:19 -0400

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,17 +2,7 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 const { exec } = require('child_process')
-exec('npm test -- -u', (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`)
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`)
-        return;
-    }
-    console.log(`stdout: ${stdout}`)
-})
+exec('npm test -- -u')
 
 describe('App', () => {
   it('Contains the compiled JavaScript', async () => {
